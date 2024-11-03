dcb04c2ceef8436badd01921a12179a649fc5d59
stoppedwumm <mobileboxnews@icloud.com>
2024-11-03 11:35:09 +0100

    Fix client problem

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const {BrowserWindow, app} = require('electron');
-const path = require('path');
-const { electron } = require('process');
+try {
+    const { BrowserWindow, app } = require('electron');
+    const path = require('path');
+    const { electron } = require('process');
+    const ws = require("ws")
 
-app.on('ready', () => {
-    require("./server")
-    let mainWindow = new BrowserWindow({
-        webPreferences: {
-            nodeIntegration: true
+    app.on('ready', () => {
+        try {
+            require("./server")
+            let mainWindow = new BrowserWindow({
+                webPreferences: {
+                    nodeIntegration: true
+                }
+            });
+            mainWindow.loadFile('index.html');
+            mainWindow.on("close", (e) => {
+                e.preventDefault()
+                app.hide()
+            })
+            const testSocket = new ws("ws://localhost:1765")
+            testSocket.addEventListener("open", () => {
+                testSocket.send("hello")
+            })
+        } catch (error) {
+            console.log(error)
         }
     });
-    mainWindow.loadFile('index.html');
-    mainWindow.on("close", (e) => {
-        e.preventDefault()
-        app.hide()
-    })
-    new WebSocket("ws://localhost:1765").onmessage = (message) => {
-        console.log(message.data)
-    }
-});
+} catch (error) {
+    console.log(error)
+}
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const ws = require("ws")
-const wsServer = new ws.Server({
-    port: 1765
-})
-
-wsServer.on("connection", (ws) => {
-    console.log("Connection established")
-    ws.on("message", (message) => {
-        console.log(message)
+try {
+    const ws = require("ws")
+    const wsServer = new ws.Server({
+        port: 1765
     })
-})
 
-module.exports = wsServer
+
+    wsServer.on("connection", (wsT) => {
+        console.log("Connection established")
+        wsT.on("message", (message) => {
+            console.log("Server message:", message.toString())
+        })
+    })
+
+    module.exports = wsServer
+} catch (error) {
+    console.log(error)
+}
